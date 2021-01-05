14e6735db5369bf3cfe1f6c5c4743c9348c2e182
Ernest Oliver Casanes <ecasanes@gmail.com>
2021-01-05 16:40:26 +0800

    opt-form

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -2,6 +2,7 @@ import React from 'react'
 import faqData from '../fixtures/faq.json';
 import { Accordion } from '../components';
 import { Item } from '../components/jumbotron/styles/jumbotron';
+import OptForm from '../components/opt-form';
 
 export function FaqsContainer() {
     return (
@@ -13,6 +14,12 @@ export function FaqsContainer() {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
+
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
         </Accordion>
     )
 }
